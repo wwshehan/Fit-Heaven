6e425ab95e3bfb3a4e0089646b32707b19addf5b
Kevin Washburn <kevincwashburn@gmail.com>
2020-03-17 12:43:24 -0400

    prettified exercises.js, added exercise-route.js

--- models/exercises.js
+++ models/exercises.js
@@ -1,28 +1,27 @@
-
-
 module.exports = function(sequelize, DataTypes) {
-    var exercises = sequelize.define("exercises", {
-        id: {
-            type: DataTypes.INT,
-            allowNull: false,
-            autoIncrement: true,
-            primaryKey: true
-        },
-        name: {
-            type: DataTypes.STRING(30),
-            allowNull: false
-        },
-        level: {
-            type: DataTypes.STRING(30),
-            allowNull: false
-        },
-        muscle: {
-            type: DataTypes.STRING(20),
-            allowNull: false
-        },
-        equipment: {
-            type: DataTypes.STRING(20),
-            allowNull: false
-        }
-    })
-}
+  var Exercises = sequelize.define("Exercises", {
+    id: {
+      type: DataTypes.INT,
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true
+    },
+    name: {
+      type: DataTypes.STRING(30),
+      allowNull: false
+    },
+    level: {
+      type: DataTypes.STRING(30),
+      allowNull: false
+    },
+    muscle: {
+      type: DataTypes.STRING(20),
+      allowNull: false
+    },
+    equipment: {
+      type: DataTypes.STRING(20),
+      allowNull: false
+    }
+  });
+  return Exercises;
+};
