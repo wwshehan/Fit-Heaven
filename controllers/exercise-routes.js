72b5eb587bfe4665454fd8d245703a256f7cf72d
Warren Shehan <wwshehan@gmail.com>
2020-03-18 12:02:46 -0400

    updating routes

--- controllers/exercise-routes.js
+++ controllers/exercise-routes.js
@@ -1,13 +1,13 @@
 var db = require("../models");
 
 module.exports = function(app) {
-  app.get("/api/exercises/:back", function(req, res) {
+  app.get("/api/exercises/", function(req, res) {
     db.Exercises.findAll({
-      where: {
-        muscle: req.params.back
-      }
-    }).then(function(dbBack) {
-      res.JSON(dbBack);
+      // where: {
+      //   muscle: req.params.muscle
+      // }
+    }).then(function(dbMuscle) {
+      res.JSON(dbMuscle);
     });
   });
 };
