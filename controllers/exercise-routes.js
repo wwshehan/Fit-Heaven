669c55a4a6a39ad32c6747716598331adf441946
Kevin Washburn <kevincwashburn@gmail.com>
2020-03-18 09:22:53 -0400

    modified server.js so it won't drop table

--- controllers/exercise-routes.js
+++ controllers/exercise-routes.js
@@ -1,8 +1,12 @@
 var db = require("../models");
 
 module.exports = function(app) {
-  app.get("/api/back", function(req, res) {
-    db.Back.findAll({}).then(function(dbBack) {
+  app.get("/api/exercises/:back", function(req, res) {
+    db.Exercises.findAll({
+      where: {
+        muscle: req.params.back
+      }
+    }).then(function(dbBack) {
       res.JSON(dbBack);
     });
   });
