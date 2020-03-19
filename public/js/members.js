1008bb1c73265b9c7159c89cb319537caa4108ac
Zach M <zatchmdws@gmail.com>
2020-03-19 14:26:11 -0400

    Working members.js file

--- public/js/members.js
+++ public/js/members.js
@@ -2,10 +2,10 @@ $(document).ready(function() {
   // This file just does a GET request to figure out which user is logged in
   // and updates the HTML on the page
   $.get("/api/user_data").then(function(data) {
-    console.log(data);
-    $(".username")
-      .text(data.email)
-      .replace("@", "");
+    let username = data.email;
+    console.log(username);
+    let realusername = username.substr(0, username.indexOf("@"));
+    $(".username").text(realusername);
     $("#gender").text(data.gender);
     $("#weight").text(data.weight);
     $("#fitnessLevel").text(data.level);
